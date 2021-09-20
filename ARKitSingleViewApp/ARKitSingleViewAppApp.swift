a9ceecf94cd778f6781a00069eb30887fcc0eddb
rob4abcba <rob_abcba@yahoo.com>
2021-09-20 12:50:16 -0700

    Initial Commit

---
+++ ARKitSingleViewApp/ARKitSingleViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitSingleViewAppApp.swift
+//  ARKitSingleViewApp
+//
+//  Created by R on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ARKitSingleViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
